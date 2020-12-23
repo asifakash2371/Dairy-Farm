088f24c178f1a375be935876736754e0cee31fe0
Asif Akash <58027186+asifakash2371@users.noreply.github.com>
2020-12-22 23:50:22 -0500

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,29 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into userdata (firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+		$execval = $stmt->execute();
+		$stmt->close();
+		$conn->close();
+	}
+?>
+
+<html>
+<head></head>
+	<body>
+		<a href="1page.html">click Here</a>
+	</body>
+	</html>
+
